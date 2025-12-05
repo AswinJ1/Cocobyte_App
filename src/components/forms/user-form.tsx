978f590c72b78be75e1402681ecde07781086b73
AswinJ1 <aswinjhome@gmail.com>
2025-12-05 17:20:13 +0530

    revert:ddd

--- src/components/forms/user-form.tsx
+++ src/components/forms/user-form.tsx
@@ -266,7 +266,7 @@ export default function UserForm({ editingUser, onSuccess }: UserFormProps) {
         {/* Super Admin Badge - Show at top for new users */}
         {isSuperAdmin && !editingUser && (
           <Alert className="">
-              You have Super Admin privileges. You can create both Participant and Admin users.
+            You can create both Participant and Admin users.
           </Alert>
         )}
 
